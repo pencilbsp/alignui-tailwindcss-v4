e5392c08d8cd6b47ed63cc037a8b779cb46f3cf0
pencilbsp <pencil.bsp@gmail.com>
2025-03-07 17:51:49 +0700

    Remove accordion component and update dependencies in package.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Carousel from "@/components/ui/carousel";
 import CarouselSwiper from "@/components/ui/carousel-swiper";
+// import NativeCarousel from "@/components/ui/native-carousel";
 
 export default function Home() {
     return (
