d490ef5acd4d2cded35b7e469f0693e22aec4ccf
pencilbsp <pencil.bsp@gmail.com>
2025-03-22 17:47:06 +0700

    Remove unused code and imports in video player

--- components/ui/video-player.tsx
+++ components/ui/video-player.tsx
@@ -1,36 +1,13 @@
 "use client";
 
-import VideoPlayerControls from "./video-player-controls";
 import VideoPlayerLoading from "./video-player-loading";
-import { useVideoPlayer } from "./video-player-provider";
+import VideoPlayerControls from "./video-player-controls";
 
 export default function VideoPlayer() {
-    const {} = useVideoPlayer();
-
     return (
         <>
             <VideoPlayerLoading />
             <VideoPlayerControls />
         </>
     );
-
-    // return <div ref={containerRef} className="text-static-white relative h-full w-full">
-    //     <video playsInline ref={videoRef} className="h-full w-full" onContextMenu={(e) => e.preventDefault()} />
-    //     <div
-    //         className={cn("absolute top-0 left-0 flex h-full w-full flex-1 items-center justify-center", {
-    //             "invisible opacity-0": isPlaying,
-    //         })}
-    //     >
-    //         {state.isLoading ? (
-    //             <Spinner loading className="bg-static-white" size="lg" />
-    //         ) : (
-    //             <button className="cursor-pointer" type="button" onClick={handlePlay} aria-label="Play Button">
-    //                 <PlayIcon className="size-10 lg:size-12" />
-    //             </button>
-    //         )}
-    //     </div>
-    //     <div className="absolute bottom-0 left-0 flex w-full flex-col items-center select-none">
-    //         <VideoPlayerControls />
-    //     </div>
-    // </div>
 }
